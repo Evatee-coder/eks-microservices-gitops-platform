b6f14fa1c3603fa7ea6f11dc1ec3e6e29b0299b0
Evatee-coder <adetayo.eyelade2@gmail.com>
2026-09-06 03:03:43 -0300

    fix: ecr and local

--- microservices-k8s/infra/ecr.tf
+++ microservices-k8s/infra/ecr.tf
@@ -1,19 +1,19 @@
 # 
-# resource "aws_ecr_repository" "images" {
-#   for_each = local.services
+resource "aws_ecr_repository" "images" {
+  for_each = local.services
 
-#   name                 = "${var.environment}-${var.app_name}-${each.key}"
-#   force_delete         = true # ✅ add this line
-#   image_tag_mutability = "MUTABLE"
+  name                 = "${var.environment}-${var.app_name}-${each.key}"
+  force_delete         = true # ✅ add this line
+  image_tag_mutability = "MUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-#   tags = {
-#     Name = each.key
-#   }
-# }
+  tags = {
+    Name = each.key
+  }
+}
 
 
 # [id=216989097838.dkr.ecr.us-east-1.amazonaws.com/prod-craftica-frontend]
